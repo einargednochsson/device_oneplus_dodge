0125990e0b6b350ba7f01b15888034312af026a5
Einar Gednochsson <pete80pro.pb@gmail.com>
2026-03-08 14:03:53 +0000

    dodge: add BCR

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# BCR Basic Call Recorder
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3168
 TARGET_SCREEN_WIDTH := 1440
